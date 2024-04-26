63c6fa8bbc170aa9d3f62cce30c878358eee4e5b
QgQ220903 <quyquach22092003@gmail.com>
2024-04-26 14:18:48 +0700

    Cập nhật lại sidebar + thêm giao diện quản lý thiết bị

---
+++ src/main/java/com/webdemo/demospringboot/controller/DeviceController.java
@@ -0,0 +1,17 @@
+package com.webdemo.demospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DeviceController {
+    @RequestMapping("device")
+    public String index()
+    {
+        return "device";
+    }
+    @RequestMapping("device/add_device")
+    public String add_device() {
+        return "add_device";
+    }
+}
