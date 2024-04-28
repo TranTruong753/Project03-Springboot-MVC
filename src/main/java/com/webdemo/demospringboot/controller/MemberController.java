cdee0861816e6ccbb04050947fca1d65b6ff3eec
Tran Truong <chantruong753@gmail.com>
2024-04-28 18:29:53 +0700

    cap nhat mot duon dan

--- src/main/java/com/webdemo/demospringboot/controller/MemberController.java
+++ src/main/java/com/webdemo/demospringboot/controller/MemberController.java
@@ -1,19 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.webdemo.demospringboot.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-/**
- *
- * @author Admin
- */
+
 @Controller
 public class MemberController {
-    
-    @RequestMapping("/member")
-    public String index() {
-        return "MemberPage";
-    }
+    @RequestMapping("member")
+    public String index() {return "member";}
+
+    @RequestMapping("member/add_member")
+    public String add_member() {return "add_member";}
+
 }
