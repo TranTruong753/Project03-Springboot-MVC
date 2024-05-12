be47b8c488e95e956d02039171a86393ef2c8fc3
Tran Truong <chantruong753@gmail.com>
2024-05-12 20:21:34 +0700

    fix change pass

--- src/main/java/com/webdemo/demospringboot/controller/ChangePasswordController.java
+++ src/main/java/com/webdemo/demospringboot/controller/ChangePasswordController.java
@@ -6,14 +6,18 @@ package com.webdemo.demospringboot.controller;
 
 import com.webdemo.demospringboot.model.Thanhvien;
 import com.webdemo.demospringboot.service.ThanhVienService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -32,7 +36,7 @@ public class ChangePasswordController {
 
     @GetMapping({"", "/changePassword"})
     public String show(Model model) {
-         String maTVString = (String) httpSession.getAttribute("maTV");
+        String maTVString = (String) httpSession.getAttribute("maTV");
         int maTV = Integer.parseInt(maTVString);
        
         String password = tvservice.search(maTV).getPassword();
@@ -41,16 +45,30 @@ public class ChangePasswordController {
         return "ChangePassword";
     }
 
-    @RequestMapping("GetPassword")
-    public String getnewpass(@RequestParam(name="id") String id ,Model model,@RequestParam(name="newpass") String newpass ) {
-        
+//    @RequestMapping("GetPassword")
+//    public String getnewpass(@RequestParam(name="id") String id ,Model model,@RequestParam(name="newpass") String newpass ) {
+//        
+//        Thanhvien tv= tvservice.search(Integer.parseInt(id));
+//        tv.setPassword(newpass);
+//        tvservice.save(tv);
+//        
+//        model.addAttribute("user", id);
+//        model.addAttribute("password", newpass);
+//        model.addAttribute("message", "Đổi mật khẩu thành công!");
+//        return "ChangePassword";
+//    }
+    
+    @RequestMapping(value = "/change", method = RequestMethod.POST)
+    public @ResponseBody String change(HttpServletRequest request) {
+        String id = request.getParameter("id");
+        String newpass = request.getParameter("code");
+        int maTV = Integer.parseInt(id);
         Thanhvien tv= tvservice.search(Integer.parseInt(id));
         tv.setPassword(newpass);
         tvservice.save(tv);
+        String mess = "Thay đổi mật khẩu thành công!";
         
-        model.addAttribute("user", id);
-        model.addAttribute("password", newpass);
-        model.addAttribute("message", "Đổi mật khẩu thành công!");
-        return "ChangePassword";
+        return mess;
+            
     }
 }
