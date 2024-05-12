b900cd46ad38417f503b000374f97770d9a0282a
nguyencongduc <nguyencongduc18THD@gmail.com>
2024-05-12 23:40:20 +0700

    push phần mượn thiết bị

---
+++ src/main/java/com/webdemo/demospringboot/repository/MuonTraRepository.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.webdemo.demospringboot.repository;
+
+import com.webdemo.demospringboot.model.ThongTinSD;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author DELL
+ */
+@Repository
+public interface MuonTraRepository extends JpaRepository<ThongTinSD, Integer>{
+    @Query("SELECT ttsd FROM ThongTinSD ttsd " +
+           "INNER JOIN ttsd.thietBi tb " +
+           "INNER JOIN ttsd.thanhVien tv " +
+           "WHERE  ttsd.thoiGianMuon IS NOT NULL ")
+    List<ThongTinSD> findAllThongTinSD();
+    
+    @Query("SELECT t FROM ThongTinSD t  WHERE t.thoiGianTra IS NULL AND t.thoiGianMuon IS NOT NULL")
+    List<ThongTinSD> findTTSDDangMuon();
+}
